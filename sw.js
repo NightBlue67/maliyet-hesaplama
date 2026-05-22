f57a486ab062b389a02ea8db67e420e89ea91b34
NightBlue67 <taskinozdemir9525@gmail.com>
2026-05-22 20:33:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,46 @@
-const CACHE_NAME = 'bristol-v1';
+const CACHE_NAME = 'bristol-v3';
+
 const ASSETS = [
-  'index.html',
-  'apple-touch-icon.png',
-  'manifest.json'
+  './',
+  './index.html',
+  './apple-touch-icon.png',
+  './manifest.json'
 ];
 
-// Dosyaları telefona yükleme (Cache)
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+// Kurulum
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
   );
+
+  // Yeni SW hemen aktif olsun
+  self.skipWaiting();
 });
 
-// İnternet yoksa telefondaki dosyaları açma
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+// Eski cache temizleme
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
+  );
+
+  self.clients.claim();
+});
+
+// Dosya çağırma
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
